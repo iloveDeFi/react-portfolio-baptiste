9ba8d8156f5d322a02e3032b8b993fabee8b9b3b
Baptiste Bessard <baptistebessard@MacBook-Air-de-Baptiste.local>
2022-08-10 16:44:58 +0200

    finished correction

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,7 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+
 // Import Swiper React components
 import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -42,25 +43,23 @@ const Testimonials = () => {
   return (
     <section id='testimonials'>
       <h5>Reviews from clients</h5>
-      <h2>Testimonials</h2>
+      <h2>Testimonials</h2> 
 
       <Swiper className="container testimonials__container" 
-      module={[Pagination]}
+      modules={[Pagination]}
       spaceBetween={40}
       slidesPerView={1}
       pagination={{clickable: true}}>
-
-      
-        {
+              {
           data.map(({avatar, name, review}, index) => {
             return (
-              <SwiperSlide key={index} className="testimonial">
+          <SwiperSlide key={index} className="testimonial">
           <div className="client__avatar">
-            <img src={avatar} alt="Avatar One" />
+            <img src={avatar}/>
           </div>
           <h5 className='client__name'>{name}</h5>
             <small className='client__review'>{review}</small>
-            </SwiperSlide>
+          </SwiperSlide>
             )
           })
         }
